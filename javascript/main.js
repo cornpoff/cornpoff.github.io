32b12bc183a2e6b07b58ae6ce0b638cfdf3dd609
cornpoff <87487274+cornpoff@users.noreply.github.com>
2021-10-20 01:03:17 -0400

    Added more content, layout adjustments

--- javascript/main.js
+++ javascript/main.js
@@ -16,20 +16,8 @@ $(document).ready(function(){
 	      lightboxImg.src = img.src;
 	   }
 	};
+
+	$(".togglebutton").click(function(){
+		$(".authorbox").toggle();
+	})
 });
-
-
-// window.onload = function() {
-// 	var modal = document.getElementById("myModal");
-// 	var img = document.getElementById("myImg");
-// 	var modalImg = document.getElementById("img01");
-// 	img.onclick = function(){
-// 	  modal.style.display = "block";
-// 	  modalImg.src = this.src;
-// 	}
-// 	var span = document.getElementsByClassName("close")[0];
-
-// 	span.onclick = function() { 
-// 	  modal.style.display = "none";
-// 	}
-// }
